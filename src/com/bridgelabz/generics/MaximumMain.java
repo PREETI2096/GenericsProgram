08343696e1b164dd18a42d4f0e3fca2857139523
PREETI2096 <preetirg96@gmail.com>
2023-01-30 09:39:57 +0530

    Added : maximum float program

--- src/com/bridgelabz/generics/MaximumMain.java
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 
 public class MaximumMain {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		Maximum obj = new Maximum();
-		
-		System.out.println("Enter first number:");
-		int n1 = sc.nextInt();
-		System.out.println("Enter second number:");
-		int n2 = sc.nextInt();
-		System.out.println("Enter third number:");
-		int n3 = sc.nextInt();
-		sc.close();
-		
-		obj.findMax(n1, n2, n3);
-		
-	}
 
+		System.out.println("Enter first number:");
+		float n1 = sc.nextFloat();
+		System.out.println("Enter second number:");
+		float n2 = sc.nextFloat();
+		System.out.println("Enter third number:");
+		float n3 = sc.nextFloat();
+		sc.close();
+		obj.findMax(n1, n2, n3);
+	}
 }
