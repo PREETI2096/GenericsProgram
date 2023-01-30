cc5f0f0d462366f3be13ab3a4a56491866421679
PREETI2096 <preetirg96@gmail.com>
2023-01-30 09:52:56 +0530

    Added : maximum string program

--- src/com/bridgelabz/generics/MaximumMain.java
+++ src/com/bridgelabz/generics/MaximumMain.java
@@ -3,18 +3,12 @@ package com.bridgelabz.generics;
 import java.util.Scanner;
 
 public class MaximumMain {
+	
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
+		
 		Maximum obj = new Maximum();
-
-		System.out.println("Enter first number:");
-		float n1 = sc.nextFloat();
-		System.out.println("Enter second number:");
-		float n2 = sc.nextFloat();
-		System.out.println("Enter third number:");
-		float n3 = sc.nextFloat();
-		sc.close();
-		obj.findMax(n1, n2, n3);
+	
+		obj.findMax("Aeroplane", "Helicopter", "Airjet");
+		
 	}
 }
